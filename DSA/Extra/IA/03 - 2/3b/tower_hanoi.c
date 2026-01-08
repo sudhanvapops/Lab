89f2a0b333d68182ce8e7be30648a9067c4c4734
Sudhanva <sudhanvapops@gmail.com>
2026-01-08 07:59:01 +0530

    DSA Changes

---
+++ DSA/Extra/IA/03 - 2/3b/tower_hanoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>   
+
+void tower(int n, char src, char temp, char dest) {
+    if (n == 0)
+        return;
+    
+    tower(n - 1, src, dest, temp);
+    printf("\nMove disc %d from %c to %c", n, src, dest);
+    tower(n - 1, temp, src, dest);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of discs: ");
+    scanf("%d", &n);
+
+    printf("\nSteps:\n");
+    tower(n, 'A', 'B', 'C');
+
+    printf("\n\nTotal number of moves: %d\n", (int)pow(2, n) - 1);
+    return 0;
+}
